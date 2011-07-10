1b2c7bdcd5fad880354f7f15b0c352329843fd27
Matt Rude <matt@mattrude.com>
2011-07-09 23:25:51 -0500

    Correct table layouts

--- inc/domain.inc.php
+++ inc/domain.inc.php
@@ -11,27 +11,31 @@ echo "<h2>Options and Users in Domain $domain</h2>";
 $sqlShowBlocked = "SELECT * FROM domain WHERE domain = '$domain';";
 $result = $dbHandle->query($sqlShowBlocked);
 $entry = $result->fetch();
-  $description = $entry['description'];
-  $aliases = $entry['aliases'];
-  $mailboxes = $entry['mailboxes'];
-  $maxquota = $entry['maxquota'];
-  $quota = ByteSize($entry['quota']);
-  $transport = $entry['transport'];
-  $backupmx = $entry['backupmx'];
-  $active = $entry['active'];
-  if ($backupmx == 'on') {$backupmx='yes';} else {$backupmx='no';}
-  if ($active == 'on') {$active='yes';} else {$active='no';}
+
+$description = $entry['description'];
+$aliases = $entry['aliases'];
+$mailboxes = $entry['mailboxes'];
+$maxquota = $entry['maxquota'];
+$quota = ByteSize($entry['quota']);
+$transport = $entry['transport'];
+$backupmx = $entry['backupmx'];
+$active = $entry['active'];
+if ($backupmx == 'on') {$backupmx='yes';} else {$backupmx='no';}
+if ($active == 'on') {$active='yes';} else {$active='no';}
 
 echo "<strong>Domain Name: </strong>$domain<br />";
 echo "<strong>Domain Description: </strong>$description";
-echo "<table><tr><td><table border='0'>";
+echo "<table border='0'>";
 echo "<tr><td>Max Number of Aliases: </td><td>$aliases</td><td>Max Number of Mailboxes: </td><td>$mailboxes</td></tr>";
 echo "<tr><td>Default User Quota: </td><td>$quota</td><td>Default Transport: </td><td>$transport</td></tr>";
 echo "<tr><td>Backup MX Domain: </td><td>$backupmx</td><td>Domain Active: </td><td>$active</td></tr>";
 echo "</td></tr></table>";
-
 echo "<a href='index.php?page=edit_domain&domain=$domain'>Edit Domain</a> <a href='index.php'>Back to Domain List</a><br />";
-echo "<table><tr><td>";
+
+
+######################################################################
+## Users in the Domain
+
 echo "<h3>Users in Domain</h3>";
 echo "<a href='index.php?page=create_user&domain=$domain'>Add User</a><br />";
 echo "<table border='0'><tr><td></td><td>Name:</td><td>Email Address:</td><td>Mail Directory:</td><td>Quota:</td><td>Modified Last:</td><td>Active:</td><td></td><td></tr>";
@@ -52,7 +56,10 @@ while ($entry2 = $result2->fetch()) {
 }
 echo "</td></tr></table>";
 
-// Aliases
+
+######################################################################
+## Aliases in the Domain
+
 echo "<h3>Aliases in Domain</h3>";
 echo "<a href='add_alias.php?domain=$domain'>Add Alias</a><br />";
 echo "<table border='0'>";
@@ -72,7 +79,8 @@ while ($entry5 = $result5->fetch()) {
   $line_count2++;
   echo "<tr bgcolor='$row_color'><td>$line_count2</td><td><a href='index.php?page=edit_alias&?domain=" .$domain. "&address=" .$address. "'>$address</a></td><td>$goto</td><td><small>$modified<small></td><td><center><a href='activate_alias.php?switch_active=$switch_active&address=$address&domain=$domain'><div id=$active></div></a></center></td><td><a href='del_alias.php?address=$address&domain=$domain'><img border=0 src='images/icon_del.png'></a></td></tr>";
 }
-echo "</table></pre></td></tr></table>";
+echo "</table>";
+
 
 include_once('functions.inc.php');
 ?>
